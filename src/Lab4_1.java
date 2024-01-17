61aeeb24aa7b4485afb46338e0c57402509f2824
kra1exx <egor14375@gmail.com>
2024-01-18 08:26:53 +0900

    Initial commit

---
+++ src/Lab4_1.java
@@ -0,0 +1,11 @@
+public class Lab4_1{
+    public static void main(String[] args) {
+        try {
+            System.out.println("0");
+            throw new RuntimeException("ошибка");
+        } catch (RuntimeException e) {
+            System.out.println("1 " + e);
+        }
+        System.out.println("2");
+    }
+}
